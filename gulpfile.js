c94635396ef83752fdf275f95f6b4cb87f030bc2
ccbikai <ccbikai@qq.com>
2015-12-23 22:25:46 +0800

    优化字体 滚动

--- gulpfile.js
+++ gulpfile.js
@@ -1,125 +1,143 @@
 var gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	autoprefixer = require('gulp-autoprefixer'),
-	minifycss = require('gulp-minify-css'),
-	uglify = require('gulp-uglify'),
-	rename = require('gulp-rename'),
-	jade = require('gulp-jade'),
-	webserver = require('gulp-webserver'),
-	concat = require('gulp-concat'),
-	fontmin = require('gulp-fontmin'),
-	shelljs = require('shelljs');
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    jade = require('gulp-jade'),
+    webserver = require('gulp-webserver'),
+    concat = require('gulp-concat'),
+    fontmin = require('gulp-fontmin'),
+    shelljs = require('shelljs');
 
+// 这种写配置的方法都不忍心看了
 var styles = [
-	'src/scss/style.scss'
+    'src/scss/style.scss'
 ];
 
 var skrollr = [
-	'src/skrollr/skrollr.css',
-	'src/skrollr/skrollr-lg.css'
+    'src/skrollr/skrollr.css',
+    'src/skrollr/skrollr-lg.css'
 ];
 
 var scripts = [
-	// zeptojs
-	'src/vendor/zeptojs/src/zepto.js',
-	//'src/vendor/zeptojs/src/data.js',
-	'src/vendor/zeptojs/src/event.js',
-	'src/vendor/zeptojs/src/ie.js',
-	'src/js/main.js',
-	// skrollr
-	'src/vendor/skrollr-stylesheets/src/skrollr.stylesheets.js',
-	'src/vendor/skrollr/src/skrollr.js'
+
+    // zeptojs
+    'src/vendor/zeptojs/src/zepto.js',
+
+    //'src/vendor/zeptojs/src/data.js',
+    'src/vendor/zeptojs/src/event.js',
+    'src/vendor/zeptojs/src/ie.js',
+    'src/js/main.js',
+
+    // skrollr
+    'src/vendor/skrollr-stylesheets/src/skrollr.stylesheets.js',
+    'src/vendor/skrollr/src/skrollr.js'
 ];
 
 var jades = [
-	'src/jade/*.jade'
+    'src/jade/*.jade'
 ];
 
-gulp.task('styles', function () {
-	'use strict';
-	return gulp.src(styles)
-		.pipe(sass())
-		.pipe(concat('style.css'))
-		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-		.pipe(rename({suffix: '.min'}))
-		.pipe(minifycss())
-		.pipe(gulp.dest('output/css'));
+gulp.task('styles', function() {
+    'use strict';
+    return gulp.src(styles)
+    .pipe(sass())
+    .pipe(concat('style.css'))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(rename({
+        suffix: '.min'
+    }))
+        .pipe(minifycss())
+        .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('skrollr', function () {
-	'use strict';
-	return gulp.src(skrollr)
-		.pipe(rename({suffix: '.min'}))
-		.pipe(minifycss({restructuring: false, compatibility: '-properties.zeroUnits'}))
-		.pipe(gulp.dest('output/css'));
+gulp.task('skrollr', function() {
+    'use strict';
+    return gulp.src(skrollr)
+    .pipe(rename({
+        suffix: '.min'
+    }))
+    .pipe(minifycss({
+        restructuring: false,
+        compatibility: '-properties.zeroUnits'
+    }))
+        .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('scripts', function () {
-	'use strict';
-	return gulp.src(scripts)
-		.pipe(concat('main.js'))
-		.pipe(rename({suffix: '.min'}))
-		.pipe(uglify())
-		.pipe(gulp.dest('output/js'));
+gulp.task('scripts', function() {
+    'use strict';
+    return gulp.src(scripts)
+    .pipe(concat('main.js'))
+    .pipe(rename({
+        suffix: '.min'
+    }))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('jades', function () {
-	'use strict';
-	return gulp.src(jades)
-		.pipe(jade())
-		.pipe(gulp.dest('output/'));
+gulp.task('jades', function() {
+    'use strict';
+    return gulp.src(jades)
+    .pipe(jade())
+    .pipe(gulp.dest('dist/'));
 });
 
 
-gulp.task('copy', function () {
-	'use strict';
-	return gulp.src('src/static/**/*')
-		.pipe(gulp.dest('output'));
+gulp.task('copy', function() {
+    'use strict';
+    return gulp.src('src/static/**/*')
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('font', function () {
-	// Nexa
-	gulp.src('src/fonts/Nexa*.ttf')
-		.pipe(fontmin())
-		.pipe(gulp.dest('src/static/font'));
-	// FFF-Tusj
-	gulp.src('src/fonts/FFF-Tusj.ttf')
-		.pipe(fontmin({
-			text: 'BiKaiTimelineSkillsHobbiesConnectProjects'
-		}))
-		.pipe(gulp.dest('src/static/font'));
+gulp.task('font', function() {
+
+    // Muli
+    gulp.src('src/fonts/Muli*.ttf')
+    .pipe(fontmin())
+    .pipe(gulp.dest('src/static/font'));
+
+    // Fredericka-the-Great
+    gulp.src('src/fonts/Fredericka-the-Great.ttf')
+        .pipe(fontmin({
+            text: 'BiKai Timeline Skills Hobbies Connect Projects'
+        }))
+        .pipe(gulp.dest('src/static/font'));
 });
 
-gulp.task('webserver', function () {
-	return gulp.src('output')
-		.pipe(webserver({
-			host: '0.0.0.0',
-			port: 5000,
-			livereload: true,
-			directoryListing: true,
-			open: 'http://127.0.0.1:5000/index.html'
-		}));
+gulp.task('webserver', function() {
+    return gulp.src('dist')
+        .pipe(webserver({
+            host: '0.0.0.0',
+            port: 5000,
+            livereload: true,
+            directoryListing: true,
+            open: 'http://127.0.0.1:5000/index.html'
+        }));
 });
 
-gulp.task('build', ['styles', 'skrollr', 'scripts', 'jades', 'font'], function () {
-	return gulp.start('copy');
+gulp.task('build', ['styles', 'skrollr', 'scripts', 'jades', 'font'], function() {
+    return gulp.start('copy');
 });
 
-gulp.task('git', function () {
-	// gulp git -m test
-	var commit = process.argv[4];
-	shelljs.exec('git add -A .; git commit -m "' + commit + '"; git push gitcafe master ; git push github master');
+gulp.task('git', function() {
+
+    // gulp git -m test
+    var commit = process.argv[4];
+
+    shelljs.exec('git add -A .; git commit -m "' + commit + '"; git push gitcafe master ; git push github master');
 });
 
-gulp.task('publish', ['build'], function () {
-	// gulp publish
-	shelljs.exec('cd output; git add -A  .; git commit -m "auto publish"; git push gitcafe gh-pages ; git push github gh-pages; cd ..');
+gulp.task('publish', ['build'], function() {
+
+    // gulp publish
+    shelljs.exec('cd dist; git add -A  .; git commit -m "auto publish"; git push gitcafe gh-pages ; git push github gh-pages; cd ..');
 });
 
-gulp.task('default', ['styles', 'skrollr', 'scripts', 'jades', 'copy', 'webserver'], function () {
-	'use strict';
-	gulp.watch('src/scss/**/*.scss', ['styles']);
-	gulp.watch('src/skrollr/*.css', ['skrollr']);
-	gulp.watch(scripts, ['scripts']);
-	gulp.watch('src/jade/**/*.jade', ['jades']);
+gulp.task('default', ['font', 'styles', 'skrollr', 'scripts', 'jades', 'copy', 'webserver'], function() {
+    'use strict';
+    gulp.watch('src/scss/**/*.scss', ['styles']);
+    gulp.watch('src/skrollr/*.css', ['skrollr']);
+    gulp.watch(scripts, ['scripts']);
+    gulp.watch('src/jade/**/*.jade', ['jades']);
 });
--- src/js/main.js
+++ src/js/main.js
@@ -1,172 +1,192 @@
 var Site = {};
-Site.launchFullscreen = function (element) {
-	if (element.requestFullscreen) {
-		element.requestFullscreen();
-	} else if (element.mozRequestFullScreen) {
-		element.mozRequestFullScreen();
-	} else if (element.webkitRequestFullscreen) {
-		element.webkitRequestFullscreen();
-	} else if (element.msRequestFullscreen) {
-		element.msRequestFullscreen();
-	}
+
+Site.launchFullscreen = function(element) {
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullscreen) {
+        element.webkitRequestFullscreen();
+    } else if (element.msRequestFullscreen) {
+        element.msRequestFullscreen();
+    }
 };
 
-Site.exitFullscreen = function () {
-	if (document.exitFullscreen) {
-		document.exitFullscreen();
-	} else if (document.mozCancelFullScreen) {
-		document.mozCancelFullScreen();
-	} else if (document.webkitExitFullscreen) {
-		document.webkitExitFullscreen();
-	}
+Site.exitFullscreen = function() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+    }
 };
 
 //http://stackoverflow.com/questions/5680013/how-to-be-notified-once-a-web-font-has-loaded
-Site.waitForWebfonts = function (fonts, callback) {
-	var loadedFonts = 0;
+Site.waitForWebfonts = function(fonts, callback) {
+    var loadedFonts = 0;
+    var i;
 
-	function doNode(font) {
-		var node = document.createElement('span');
-		// Characters that vary significantly among different fonts
-		node.innerHTML = 'BiKai\ue800';
-		// Visible - so we can measure it - but not on the screen
-		node.style.position = 'absolute';
-		node.style.left = '-10000px';
-		node.style.top = '-10000px';
-		// Large font size makes even subtle changes obvious
-		node.style.fontSize = '300px';
-		// Reset any font properties
-		node.style.fontFamily = 'sans-serif';
-		node.style.fontVariant = 'normal';
-		node.style.fontStyle = 'normal';
-		node.style.fontWeight = 'normal';
-		node.style.letterSpacing = '0';
-		document.body.appendChild(node);
-		// Remember width with no applied web font
-		var width = node.offsetWidth;
-		node.style.fontFamily = font;
-		var interval;
+    function doNode(font) {
+        var node = document.createElement('span');
+        var width = node.offsetWidth;
+        var interval;
 
-		function checkFont() {
-			// Compare current width with original width
-			if (node && node.offsetWidth !== width) {
-				++loadedFonts;
-				node.parentNode.removeChild(node);
-				node = null;
-			}
-			// If all fonts have been loaded
-			if (loadedFonts >= fonts.length) {
-				if (interval) {
-					clearInterval(interval);
-				}
-				if (loadedFonts === fonts.length) {
-					callback();
-					return true;
-				}
-			}
-		}
+        // Characters that vary significantly among different fonts
+        node.innerHTML = 'BiKai\ue800';
 
-		if (!checkFont()) {
-			interval = setInterval(checkFont, 50);
-		}
-	}
+        // Visible - so we can measure it - but not on the screen
+        node.style.position = 'absolute';
+        node.style.left = '-10000px';
+        node.style.top = '-10000px';
 
-	for (var i = 0, l = fonts.length; i < l; ++i) {
-		doNode(fonts[i]);
-	}
+        // Large font size makes even subtle changes obvious
+        node.style.fontSize = '300px';
+
+        // Reset any font properties
+        node.style.fontFamily = 'sans-serif';
+        node.style.fontVariant = 'normal';
+        node.style.fontStyle = 'normal';
+        node.style.fontWeight = 'normal';
+        node.style.letterSpacing = '0';
+        document.body.appendChild(node);
+
+        // Remember width with no applied web font
+        width = node.offsetWidth;
+
+        node.style.fontFamily = font;
+
+        function checkFont() {
+
+            // Compare current width with original width
+            if (node && node.offsetWidth !== width) {
+                ++loadedFonts;
+                node.parentNode.removeChild(node);
+                node = null;
+            }
+
+            // If all fonts have been loaded
+            if (loadedFonts >= fonts.length) {
+                if (interval) {
+                    clearInterval(interval);
+                }
+                if (loadedFonts === fonts.length) {
+                    callback();
+                    return true;
+                }
+            }
+        }
+
+        if (!checkFont()) {
+            interval = setInterval(checkFont, 50);
+        }
+    }
+
+    for (i = 0, l = fonts.length; i < l; ++i) {
+        doNode(fonts[i]);
+    }
 };
-Site.show = function () {
-	$('.loading').parent('.mask').addClass('fadeOut');
-	setTimeout(function () {
-		$('.loading').parent('.mask').css('display', 'none');
-	}, 1000);
+Site.show = function() {
+    $('.loading').parent('.mask').addClass('fadeOut');
+    setTimeout(function() {
+        $('.loading').parent('.mask').css('display', 'none');
+    }, 1000);
 };
-Site.init = function () {
-	if (Site.skrollr) {
-		Site.skrollr.refresh();
-	}
-	else {
-		Site.skrollr = skrollr.init({
-			forceHeight: false
-		});
-	}
-	document.addEventListener("visibilitychange", function () {
-		if (document.hidden) {
-			document.title = '别忘了回来再看看简历';
-		} else {
-			document.title = '简历';
-		}
-	});
-	Site.waitForWebfonts(['Nexa', 'FFF-Tusj', 'resume-icon'], function () {
-		Site.show();
-	});
-	setTimeout(function () {
-		// 字体超时加载超时后,依然显示站点
-		Site.show();
-	}, 10000);
+Site.init = function() {
+    if (Site.skrollr) {
+        Site.skrollr.refresh();
+    } else {
+        Site.skrollr = skrollr.init({
+            forceHeight: false,
+            smoothScrolling: true,
+            smoothScrollingDuration: 200,
+            mobileDeceleration: 0.001
+        });
+    }
+    document.addEventListener("visibilitychange", function() {
+        if (document.hidden) {
+            document.title = '别忘了回来再看看简历';
+        } else {
+            document.title = '简历';
+        }
+    });
+    Site.waitForWebfonts(['Muli-Light', 'Fredericka-the-Great', 'resume-icon'], function() {
+        Site.show();
+    });
+    setTimeout(function() {
+
+        // 字体超时加载超时后,依然显示站点
+        Site.show();
+    }, 5000);
 };
 
-$(function () {
-	// TODO: 增加项目
-	var $email = $('.email'),
-		$qrcode = $('.qrcode'),
-		realMail = $email.data('text').join(''),
-		fullscreenEnabled;
+$(function() {
 
-	$email.text(realMail).parent('.connect-item').attr({'href': 'mailto:' + realMail});
+    // TODO: 增加项目
+    var $email = $('.email'),
+    $qrcode = $('.qrcode'),
+    realMail = $email.data('text').join(''),
+    fullscreenEnabled;
 
-	$('.resume').on('click', function () {
-		if (fullscreenEnabled) {
-			Site.exitFullscreen();
-			fullscreenEnabled = false;
-			_hmt.push(['_trackEvent', 'resume', 'exitFullscreen', 'resume', '1']);
-		}
-		else {
-			Site.launchFullscreen(document.documentElement);
-			fullscreenEnabled = true;
-			_hmt.push(['_trackEvent', 'resume', 'launchFullscreen', 'resume', '1']);
-		}
-	});
+    $email.text(realMail).parent('.connect-item').attr({
+        href: 'mailto:' + realMail
+    });
 
-	$('.skill-bar').forEach(function (bar) {
-		bar = $(bar);
-		bar.css('width', bar.data('width'));
-	});
+    $('.resume').on('click', function() {
+        if (fullscreenEnabled) {
+            Site.exitFullscreen();
+            fullscreenEnabled = false;
+            _hmt.push(['_trackEvent', 'resume', 'exitFullscreen', 'resume', '1']);
+        } else {
+            Site.launchFullscreen(document.documentElement);
+            fullscreenEnabled = true;
+            _hmt.push(['_trackEvent', 'resume', 'launchFullscreen', 'resume', '1']);
+        }
+    });
 
-	$('.trip-item').on('click', function () {
-		$(this).next('.trip-item-detail').addClass('zoomIn').show();
-		_hmt.push(['_trackEvent', 'trip-item', 'click', $(this).find('.job').text(), '1']);
-	});
+    $('.skill-bar').forEach(function(bar) {
+        var $bar = $(bar);
 
-	$('.trip-item-close').on('click', function () {
-		$(this).parent('.trip-item-detail').removeClass('zoomIn').hide();
-	});
+        $bar.css('width', $bar.data('width'));
+    });
 
-	$('.rocket').on('click', function () {
-		var $rocket = $(this);
-		$rocket.addClass('launch');
-		Site.skrollr.animateTo(0, {duration: 600});
-		setTimeout(function () {
-			$rocket.removeClass('launch');
-		}, 600);
-		_hmt.push(['_trackEvent', 'rocket', 'gotoTop', 'gotoTop', '0']);
-	});
+    $('.trip-item').on('click', function() {
+        $(this).next('.trip-item-detail').addClass('zoomIn').show();
+        _hmt.push(['_trackEvent', 'trip-item', 'click', $(this).find('.job').text(), '1']);
+    });
 
-	$('.wechat').on('click', function (e) {
-		$qrcode.css('display', 'block');
-		e.preventDefault();
-		_hmt.push(['_trackEvent', 'wechat', 'open', 'wechat', '1']);
-	});
+    $('.trip-item-close').on('click', function() {
+        $(this).parent('.trip-item-detail').removeClass('zoomIn').hide();
+    });
 
-	$qrcode.on('click', function () {
-		$qrcode.css('display', 'none');
-		_hmt.push(['_trackEvent', 'wechat', 'close', 'wechat', '0']);
-	});
+    $('.rocket').on('click', function() {
+        var $rocket = $(this);
 
-	$('.connect-item').on('click', function () {
-		_hmt.push(['_trackEvent', 'connect', 'click', $(this).attr('title'), '1']);
-	});
+        $rocket.addClass('launch');
+        Site.skrollr.animateTo(0, {
+            duration: 600
+        });
+        setTimeout(function() {
+            $rocket.removeClass('launch');
+        }, 600);
+        _hmt.push(['_trackEvent', 'rocket', 'gotoTop', 'gotoTop', '0']);
+    });
 
-	Site.init();
+    $('.wechat').on('click', function(e) {
+        $qrcode.css('display', 'block');
+        e.preventDefault();
+        _hmt.push(['_trackEvent', 'wechat', 'open', 'wechat', '1']);
+    });
+
+    $qrcode.on('click', function() {
+        $qrcode.css('display', 'none');
+        _hmt.push(['_trackEvent', 'wechat', 'close', 'wechat', '0']);
+    });
+
+    $('.connect-item').on('click', function() {
+        _hmt.push(['_trackEvent', 'connect', 'click', $(this).attr('title'), '1']);
+    });
+
+    Site.init();
 
 });
